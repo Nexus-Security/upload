faf3663f3a361ae0e3d78d5eb668fcb7b292d109
Nexus-Security <61948581+Nexus-Security@users.noreply.github.com>
2024-07-23 14:15:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+    <h1>Upload File</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload File" name="submit">
+    </form>
+    <?php
+    if (isset($_FILES['fileToUpload'])) {
+        $target_dir = "/var/www/html/uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+    ?>
+</body>
+</html>
